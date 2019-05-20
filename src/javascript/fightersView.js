b3af1da6e1f4d7aaba6f60ea2f7b1381ed56be68
Andrey Akimov <rojer9999@gmail.com>
2019-05-20 17:16:13 +0300

    implement handleFighterClick function

--- src/javascript/fightersView.js
+++ src/javascript/fightersView.js
@@ -1,5 +1,8 @@
 import View from './view';
 import FighterView from './fighterView';
+import Fighter from './Fighter';
+import fight from './fight.js';
+import { fighterService } from './services/fightersService';
 
 class FightersView extends View {
   constructor(fighters) {
@@ -11,22 +14,81 @@ class FightersView extends View {
 
   fightersDetailsMap = new Map();
 
+  fightFunc;
+
   createFighters(fighters) {
     const fighterElements = fighters.map(fighter => {
       const fighterView = new FighterView(fighter, this.handleClick);
       return fighterView.element;
     });
 
-    this.element = this.createElement({ tagName: 'div', className: 'fighters' });
+    this.element = this.createElement({ tagName: 'div', classNames: ['fighters'] });
     this.element.append(...fighterElements);
   }
 
-  handleFighterClick(event, fighter) {
-    this.fightersDetailsMap.set(fighter._id, fighter);
-    console.log('clicked')
-    // get from map or load info and add to fightersMap
-    // show modal with fighter info
-    // allow to edit health and power in this modal
+  async handleFighterClick(event, fighter) {
+    try {
+      // map a fighter
+      const { _id } = fighter;
+      const thisMap = this.fightersDetailsMap;
+      if (!thisMap.has(_id) || !thisMap.get(_id).health) {
+        const fighterDetails = await fighterService.getFighterDetails(_id);
+        thisMap.set(_id, { ...fighter, ...fighterDetails });
+      }
+
+      // modal
+      document.getElementById('dialog-dark').showModal();
+      const currentFighter = thisMap.get(_id);
+      const temporaryData = { ...currentFighter };
+      const { name, health, attack, defense } = temporaryData;
+  
+      const calculateProgressBar = (elementId, fighterProp, maxVal) => {
+        const progressBar = document.getElementById(elementId);
+        progressBar.value = fighterProp;
+        progressBar.max = maxVal ? maxVal : fighterProp * 2;
+      }
+      calculateProgressBar('health-progress', health);
+      calculateProgressBar('attack-progress', attack);
+      calculateProgressBar('defense-progress', defense, 4);
+
+      const addProgressBtnListener = (action, prop) => {
+        const evListener = () => {
+          if ((temporaryData[prop] <= 1 && action === 'minus')
+            || (temporaryData[prop] >= currentFighter[prop] * 2 && action !== 'minus')){
+              return false;
+            }
+          temporaryData[prop] = action === 'minus' ? temporaryData[prop] - 1 : temporaryData[prop] + 1;
+          document.getElementById(`${prop}-progress`).value = temporaryData[prop];
+        }
+        document.getElementById(`${action}-${prop}`).addEventListener('click', evListener, false);
+      }
+      addProgressBtnListener('minus', 'health');
+      addProgressBtnListener('plus', 'health');
+      addProgressBtnListener('minus', 'attack');
+      addProgressBtnListener('plus', 'attack');
+  
+      const nameInput = document.getElementById('name_field');
+      nameInput.value = name;
+
+      const selectListener = () => {
+        removeSelectListener();
+        const { health, attack } = temporaryData;
+        const fighter = new Fighter(nameInput.value, health, attack, defense);
+        if (this.fightFunc) {
+          this.fightFunc(fighter)
+        } else {
+          this.fightFunc = fight(fighter);
+        }
+      };
+      const modalSelect = document.getElementById('select-modal');
+      const removeSelectListener = () => modalSelect.removeEventListener('click', selectListener, false);
+      modalSelect.addEventListener('click', selectListener, false);
+      document.getElementById('cancel-modal').addEventListener('click', removeSelectListener, false);
+      
+    } catch (error) {
+      console.warn(error);
+    }
+    
   }
 }
 
